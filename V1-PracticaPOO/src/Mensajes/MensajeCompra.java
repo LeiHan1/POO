be2af9b72753353d190df8ff634219ce00dcfd89
sarry150 <sergiosarriaporro@gmail.com>
2018-05-31 14:39:44 +0200

    Add files via upload

--- V1-PracticaPOO/src/Mensajes/MensajeCompra.java
+++ V1-PracticaPOO/src/Mensajes/MensajeCompra.java
@@ -4,27 +4,64 @@
  * and open the template in the editor.
  */
 package Mensajes;
-
-import Banco.AgenteDeInversiones;
-
 /**
  *
  * @author Han Lei
  */
 public class MensajeCompra extends Mensaje {
+    
     private double cantidadInvertir;
+    private String nombreEmpresa;
+    private String nombreCliente;
+    private int codigo = 0;
+    private static String texto;
     
     public MensajeCompra(String nC, String nE, double cantidad) {
         this.nombreCliente = nC;
         this.nombreEmpresa = nE;
         this.cantidadInvertir = cantidad;
-        this.id = AgenteDeInversiones.getTamañoListaPeticiones();
+        this.codigo = codigo +1;
+        this.texto = (codigo + "|" + nC +"|"+ nE + "|" + cantidad);
     }
-    
-    @Override
-    public void imprimir(){
-        System.out.println ("Peticion de compra");
-        System.out.println ("ID: "+id +" Cliente: "+nombreCliente +" Empresa: "+nombreEmpresa +" Cantidad: "+cantidadInvertir);
+
+    public double getCantidadInvertir() {
+        return cantidadInvertir;
+    }
+
+    public void setCantidadInvertir(double cantidadInvertir) {
+        this.cantidadInvertir = cantidadInvertir;
+    }
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public static String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
     }
     
 }
